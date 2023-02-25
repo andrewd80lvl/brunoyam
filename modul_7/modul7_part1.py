080580ed2229c76de4ee09c3fa0590e99deff0a3
andrew.d80lvl <andrew.d80lvl@gmail.com>
2023-02-25 17:55:34 +0300

    Build Graph

--- modul_7/modul7_part1.py
+++ modul_7/modul7_part1.py
@@ -1,10 +1,10 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-
+import matplotlib.pyplot as plt
 
 def parse_html(url):
-    print(f'Start: {url}')
+    print(f'Start parse_html(url): {url}')
     r = requests.get(url)
     print(r.status_code)
 
@@ -20,6 +20,7 @@ def parse_html(url):
 
         elif re.fullmatch(r'[с][ ](0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](19|20)\d\d', tag.getText()):
             date = tag.getText()
+            date = date.removeprefix('с ')
 
         elif re.fullmatch(r'[+−]\d{1,3}\.\d{1,4}', tag.getText()):
             delta = tag.getText()
@@ -27,6 +28,26 @@ def parse_html(url):
 
     return li_rate
 
-print(parse_html("https://mfd.ru/currency/?currency=USD"))
+def build_graph(rate):
+    print(f'Start build_graph(rate)')
+
+    x = list()
+    y = list()
+    i = 0
+    for rt in rate:
+        x.append()
+        y.append(float(rt[0]))
+        i += 1
+
+    print(y)
+    plt.plot(x,y)
+    plt.show()
+
+
+rate = parse_html("https://mfd.ru/currency/?currency=USD")
+build_graph(rate)
+
+
+
 
 #https://devpractice.ru/files/books/python/Matplotlib.book.pdf
