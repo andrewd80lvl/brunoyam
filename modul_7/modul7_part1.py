d2c5da0e3ac3d8e4f13022cdbe3e6684bbd5424f
andrew.d80lvl <andrew.d80lvl@gmail.com>
2023-02-26 15:46:54 +0300

    Доработаны функции отображения графика - распределение по датам

--- modul_7/modul7_part1.py
+++ modul_7/modul7_part1.py
@@ -2,6 +2,8 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
+import datetime as dt
+
 
 def parse_html(url):
     print(f'Start parse_html(url): {url}')
@@ -16,38 +18,32 @@ def parse_html(url):
     for tag in rate.find_all('td'):
 
         if re.fullmatch(r'[*]?\d{2,4}\.\d{4}', tag.getText()):
-            rate = tag.getText()
+            it_rate = tag.getText()
 
         elif re.fullmatch(r'[с][ ](0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](19|20)\d\d', tag.getText()):
-            date = tag.getText()
-            date = date.removeprefix('с ')
+            it_date = tag.getText().removeprefix('с ')
 
         elif re.fullmatch(r'[+−]\d{1,3}\.\d{1,4}', tag.getText()):
-            delta = tag.getText()
-            li_rate.append((rate, date, delta))
+            it_delta = tag.getText()
+            li_rate.append((it_rate, it_date, it_delta))
 
     return li_rate
 
-def build_graph(rate):
+
+def build_graph(li_rate):
     print(f'Start build_graph(rate)')
 
-    x = list()
-    y = list()
-    i = 0
-    for rt in rate:
-        x.append()
+    x = []
+    y = []
+
+    for rt in li_rate:
+        x.append(dt.datetime.strptime(rt[1], '%d.%m.%Y'))
         y.append(float(rt[0]))
-        i += 1
 
-    print(y)
-    plt.plot(x,y)
+    plt.plot(x, y)
     plt.show()
-
 
 rate = parse_html("https://mfd.ru/currency/?currency=USD")
 build_graph(rate)
 
-
-
-
-#https://devpractice.ru/files/books/python/Matplotlib.book.pdf
+# https://devpractice.ru/files/books/python/Matplotlib.book.pdf
